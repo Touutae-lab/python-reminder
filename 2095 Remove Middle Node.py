60fc2a4686a6bd3d529de29ebb62a6a1ae3dc8f0
Touutae-lab <pantakan.totae@gmail.com>
2024-03-07 22:11:30 +0700

    Add 2095

---
+++ 2095 Remove Middle Node.py
@@ -0,0 +1,15 @@
+class Solution:
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        slow = head
+        fast = head
+        prev = None
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
+        
+        if prev:
+            prev.next = slow.next
+            return head
+        return None
